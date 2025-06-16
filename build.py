0d81e917bb7d55033c9c5f1a3850024132170da0
VIRTUALGUARD <virtualguard101@gmail.com>
2025-06-17 03:36:33 +0800

    "Update build scripts, abandoned os.system()"

--- build.py
+++ build.py
@@ -1,16 +1,33 @@
 #!/usr/bin/env python3
 
-import os
+import subprocess
+
+# try:
+#     os.system("uv run scripts/recent_notes.py")
+
+#     path, message = input("Commit path: "), input("Commit message: ")
+
+#     # git operation
+#     os.system(f"git add {path}")
+#     os.system(f"git commit -m \"{message}\"")
+#     os.system("git push origin main")
+
+# except KeyboardInterrupt as e:
+#     print("User Interrupted", e)
 
 try:
-    os.system("uv run scripts/recent_notes.py")
+    # Update recent notes at index
+    subprocess.run(["uv", "run", "scripts/recent_notes.py"])
 
     path, message = input("Commit path: "), input("Commit message: ")
 
-    # git operation
-    os.system(f"git add {path}")
-    os.system(f"git commit -m \"{message}\"")
-    os.system("git push origin main")
+    # Git operations
+    subprocess.run(["git", "add", f"{path}"])
+    subprocess.run(["git", "commit", "-m", f"\"{message}\""])
+    subprocess.run(["git", "push", "origin", "main"])
 
 except KeyboardInterrupt as e:
     print("User Interrupted", e)
+
+except subprocess.CalledProcessError as e:
+    print(f"Process failed with {e.returncode}: {e.stderr}")
